ae20083150f0d11a14ba9fe678484c442f6ce430
gucchisk <gucchi_sk@yahoo.co.jp>
2020-01-15 01:54:59 +0900

    Add centos7 settings

--- compile_and_link/compile_and_link.sh
+++ compile_and_link/compile_and_link.sh
@@ -10,11 +10,12 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 	VERSION=$(rpm --eval %{centos_ver})
 	if [ $VERSION -eq 6 ]; then
 	    LIBDIR="/usr/lib/gcc/x86_64-redhat-linux/4.4.7"
-	    LINKER="/lib64/ld-2.12.so"
+	elif [ $VERSION -eq 7 ]; then
+	    LIBDIR="/usr/lib/gcc/x86_64-redhat-linux/4.8.5"
 	fi
     fi
-    ld main.o /usr/lib64/{crt1.o,crti.o} $LIBDIR/{crtbegin.o,crtend.o} /usr/lib64/crtn.o -lc -lstdc++ -L$LIBDIR -o hello -dynamic-linker $LINKER
-    echo "link: ld main.o /usr/lib64/{crt1.o,crti.o} $LIBDIR/{crtbegin.o,crtend.o} /usr/lib64/crtn.o -lc -lstdc++ -L$LIBDIR -o hello -dynamic-linker $LINKER"
+    ld main.o /usr/lib64/{crt1.o,crti.o} $LIBDIR/{crtbegin.o,crtend.o} /usr/lib64/crtn.o -lc -lstdc++ -L$LIBDIR -o hello -dynamic-linker /lib64/ld-linux-x86-64.so.2
+    echo "link: ld main.o /usr/lib64/{crt1.o,crti.o} $LIBDIR/{crtbegin.o,crtend.o} /usr/lib64/crtn.o -lc -lstdc++ -L$LIBDIR -o hello -dynamic-linker /lib64/ld-linux-x86-64.so.2"
 fi
 
     
