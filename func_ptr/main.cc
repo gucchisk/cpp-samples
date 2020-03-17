8e2f152dd109a888a8fb8e5d7344b9d365eac038
gucchisk <gucchi_sk@yahoo.co.jp>
2020-03-18 02:18:20 +0900

    test

--- func_ptr/main.cc
+++ func_ptr/main.cc
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <iomanip>
+#include <cstring>
+
+#include <sys/mman.h>
+#include <stdint.h>
+
+class Unprotect
+{
+public:
+  Unprotect(void *location, size_t count)
+  : origFunc((intptr_t)location & (~0xFFF))
+  , byteCount(count + ((intptr_t)location - origFunc))
+  {
+        mprotect((void *)origFunc, this->byteCount, PROT_READ|PROT_WRITE|PROT_EXEC);
+  };
+  ~Unprotect()
+  {
+        mprotect((void *)origFunc, byteCount, PROT_READ|PROT_EXEC);
+  }
+private:
+  intptr_t origFunc;
+  int byteCount;
+};
+
+template <typename T, typename U>
+T horrible_cast(U u)
+{
+        union { T t; U u; } un;
+        un.u = u;
+        return un.t;
+}
 
 void hello() {
   std::cout << "hello" << std::endl;
@@ -10,6 +41,14 @@ void world() {
 
 int main() {
   void (*func)() = hello;
-  * (unsigned char *)func = 0xe9;
+  // void* func = horrible_cast<void *>(hello);
+  // Unprotect _allow_write((void*)func, 32);
+  // * (unsigned char *)func = 0xe9;
+
+  char *ch = "hello";
+  mprotect((void *)ch, 1, PROT_WRITE|PROT_READ);
+  *ch = '!';
+  std::cout << ch << std::endl;
+  
   return 0;
 }
